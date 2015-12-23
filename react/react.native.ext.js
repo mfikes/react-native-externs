79d8065fb2b37ed305f7e883a8aed260839b4b30
Artem Yarulin <artem.yarulin@fessguid.com>
2015-12-23 16:10:46 +0200

    Platform added

--- react/react.native.ext.js
+++ react/react.native.ext.js
@@ -1,10 +1,11 @@
 var AppRegistry = {}
-
 AppRegistry.getAppKeys = function() {}
 AppRegistry.registerRunnable = function () {}
 AppRegistry.registerComponent = function () {}
 
-
 var ReactElement = {}
-
 ReactElement.createElement = function() {}
+
+var Platform = {}
+Platform.OS = {}
+Platform.Version = {}
